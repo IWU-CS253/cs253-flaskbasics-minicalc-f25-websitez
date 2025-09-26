5c1708f38fe44cc9521f8b8a7d0b0d128bd2040c
Cass-Adams <cadams@iwu.edu>
2025-09-26 15:16:57 -0500

    added wordstats functionality

--- wordstats.py
+++ wordstats.py
@@ -5,3 +5,27 @@ def average_length(user_string):
     else:
         avg = 0
     return avg
+
+
+def word_count(user_string):
+    words = user_string.split()
+    if words:
+        total_count = len(words)
+    else:
+        total_count = 0
+    return total_count
+
+def char_count(user_string):
+    return len(user_string)
+
+def longest_word(user_string):
+    longest_word_len = 0
+    words = user_string.split()
+    long_word = None
+    for word in words:
+        if len(word) > longest_word_len:
+            long_word = word
+            longest_word_len = len(word)
+        elif len(word) == longest_word_len:
+            long_word = long_word + ", " + word
+    return long_word
